ba295db0484165ace998cbd8c973f3bad8aff26b
Ruben Mathew <ruben.mathew24@gmail.com>
2024-12-10 21:51:06 -0600

    Batch Testing
    Added Batch Testing method to app.py

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import normalize
 import ocr
+from tqdm import tqdm
 
 IMAGE = "images/Green.JPG"
 
 # Off by Default 
 USE_GPU = False
+
+TESTS = {
+    "Fleeting Spirit" : 'White.JPG',
+    "Battering Craghorn" : 'Red.JPG',
+    "Servant of the Conduit" : 'Green.JPG',
+    "Flesh to Dust" : 'Black.JPG',
+    "Blightbelly Rat" : 'Special.JPG',
+	"Cloud Elemental" : 'Blue.JPG',
+}
 
 def main():
 
@@ -25,5 +35,32 @@ def main():
 
 	return name # Placeholder until rest of method is implemented
 
+def batch_testing():
+    
+    # Testing names right now, until rest is implemented
+    passed = 0
+    failed = []
+
+    for name, file in tqdm(TESTS.items(), desc="Testing", leave=False):
+        global IMAGE
+        IMAGE = 'images/' + file
+        computed = main()
+
+        try: 
+            assert name == computed
+            passed += 1
+        except:
+            failed += [f'Known Name: {name} doesn\'t match Computed Name: {computed}']
+
+    # Passed Cases
+    percent = round(100*passed/len(TESTS.items()), 2)
+    print(f'Passed {percent}% of cases')
+
+    # Failed Cases
+    if len(failed) > 1: print("Failed Cases:")
+    for idx, item in enumerate(failed):
+        print(f'\t{idx}. {item}')
+
 if __name__ == "__main__":
-	main()
+	# main()
+    batch_testing()
